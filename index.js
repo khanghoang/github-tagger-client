5ee90eaba0556c644323ae1da3ea9d6ca6dca7a7
Khang Hoang <hoangtrieukhang@gmail.com>
2017-09-05 22:31:29 +0700

    wip/successfully fetch repos

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import { app, BrowserWindow } from 'electron';
 import { enableLiveReload } from 'electron-compile';
 import electron from 'electron';
 import auth from 'electron-auth';
+import fetch from 'node-fetch';
 /* eslint-enable */
 
 enableLiveReload({ strategy: 'react-hmr' });
@@ -33,18 +34,49 @@ app.on('ready', () => {
   mainWindow.loadURL(`file://${__dirname}/index.html`);
 
   var opt = {
-    client_id: 'a12166a8942e05cd1c4f',
-    client_secret: '20ba4d0d84fb9725b1d3f7ec324a1f53869fcfb5',
+    // client_id: 'a12166a8942e05cd1c4f',
+    // client_secret: '20ba4d0d84fb9725b1d3f7ec324a1f53869fcfb5',
+    client_id: 'aa957d0410e4f9cbdb8d',
+    client_secret: '880dad95b2bb6481b4f2b18122e7ebebed3a1c96',
   };
 
   //Handle the github authentication
-  auth(auth.providers.github, { ...opt, scope: 'user:email' }, function(error, token) {
-    //Display the error
-    console.log('Error: ');
-    console.log(error);
+  auth(
+    auth.providers.github,
+    { ...opt, scope: 'user:email' },
+    async (error, token) => {
+      console.log(
+        'token: ',
+        JSON.stringify({
+          token,
+        })
+      );
+      const rawData = await fetch(
+        'https://github-tagger.herokuapp.com/login/github',
+        {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({
+            token,
+          }),
+        }
+      );
+      const receivedToken = await rawData.json();
 
-    //Display the generated token
-    console.log('Token: ');
-    console.log(token);
-  });
+      console.log('receivedToken: ', receivedToken);
+
+      const repos = await fetch(
+        'https://github-tagger.herokuapp.com/getRepo?tag=',
+        {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${receivedToken.token}`,
+          },
+        }
+      ).then(res => res.json())
+
+      console.log('token: ', repos);
+    }
+  );
 });
